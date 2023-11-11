4c712fc55262fbd88bf1873c268595281194e1d0
Hasnathasan <hasnatoooooooo@gmail.com>
2023-11-11 10:13:42 +0600

    some changes in vercel.json index.js file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 8000;
 
